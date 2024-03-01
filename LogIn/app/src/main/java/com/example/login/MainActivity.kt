895298f9e1ef8a51278cf4df27bb930501d82a58
githubtvp <tvp1640@gmail.com>
2024-03-01 14:02:26 +0530

    Saving latest Updates with script

--- LogIn/app/src/main/java/com/example/login/MainActivity.kt
+++ LogIn/app/src/main/java/com/example/login/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn1 = findViewById<Button>(R.id.btnLogIn)
+        var loginStatus = "Succesful LogIn"
+        btn1.setOnClickListener { onClickBtnLogIn(loginStatus) }
+    }
+
+    fun onClickBtnLogIn(logInStatus: String)
+    {
+        Toast.makeText(this, "Here 1", Toast.LENGTH_LONG).show()
+        val nextPg = Intent(this, SuccessLogIn::class.java)
+        nextPg.putExtra("LogInStatusMsg", logInStatus)
+        startActivity(nextPg)
+
     }
 }
