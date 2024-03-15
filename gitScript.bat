1569b18083ba059b1ed5277932420dc730a78187
harikishore <hari2002kishore@gmail.com>
2024-03-15 14:56:45 +0530

    Saving latest Updates with script

--- gitScript.bat
+++ gitScript.bat
@@ -1,7 +1,8 @@
 ::::git pull origin main
 ::git remote add origin https://github.com/githubtvp/23MX21.git
 ::https://github.com/githubtvp/HomeTutorProtoType.git
-git pull origin main --allow-unrelated-histories
+::git pull origin main --allow-unrelated-histories
+git pull origin main
 git add .
 git commit -a -m "Saving latest Updates with script"
 git push origin main
