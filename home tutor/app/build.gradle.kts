a437d514161501b18123234ef2db2b09cd615aed
githubtvp <tvp1640@gmail.com>
2024-03-24 22:32:55 +0530

    Saving latest Updates with script

--- home tutor/app/build.gradle.kts
+++ home tutor/app/build.gradle.kts
@@ -46,35 +46,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.8.0")
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
     implementation("androidx.cardview:cardview:1.0.0")
 //added by tvp
-    implementation("androidx.cardview:cardview:1.0.0")
-=======
-<<<<<<< HEAD
->>>>>>> 683d9cc6dc59a923b4fdee781e2f0a569daa4d0f
-    implementation("androidx.cardview:cardview:1.0.0")
-//added by tvp
-    implementation("androidx.cardview:cardview:1.0.0")
-
-
-<<<<<<< HEAD
-    implementation("androidx.cardview:cardview:1.0.0")
-
-//added by tvp
-    implementation("androidx.cardview:cardview:1.0.0")
-
-
-
-=======
-=======
->>>>>>> f2c77db349dc47c3631d7b6a69f5d95691799670
->>>>>>> 7ad808199bf26cbfa2b4d2f924ec6c50426ae664
->>>>>>> 786bed812b3e68aa3b48b794aa6fdb2a227ddb3c
->>>>>>> 683d9cc6dc59a923b4fdee781e2f0a569daa4d0f
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
